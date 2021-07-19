0946916a1a2a3c44d5bb6781f8479d2322dce552
arodrigues21803823 <afonso.pitukaa@gmail.com>
2021-07-19 12:57:49 +0100

    final

--- pMentHa/views.py
+++ pMentHa/views.py
@@ -87,7 +87,8 @@ def register(request):
 
         # Attempt to create new user
         try:
-            user = User.objects.create_user(username, email, password)
+            user = User.objects.create_user(username, email, password,)
+            user.is_staff=True
             user.save()
         except IntegrityError:
             return render(request, "pMentHa/register.html", {
